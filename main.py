23d0a048960f9d183273c72d816a543e5cd2829b
Okey Onyenwe <okeyonyenwe@Okeys-MacBook-Pro.local>
2022-12-03 00:33:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+numbers = list(range(1,101))
+print(numbers)
+multiples_of_3 = list(range(3,101,3))
+multiples_of_5 = list(range(5,101,5))
+multiples_of_both= list(range(15,101,15))
+print(multiples_of_3)
+print(multiples_of_5)
+print(multiples_of_both)
+
+
+
